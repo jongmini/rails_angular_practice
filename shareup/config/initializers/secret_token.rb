5e607b0b9ff98df27b386f0cbcba65ecad1de01d
jongmini <jongmin.choi@gmail.com>
2014-05-07 23:09:20 -0700

    creating the shareup app using rails and angular

---
+++ shareup/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Shareup::Application.config.secret_key_base = '393eb385dc8b53bc1bb3a6224761b0de7b46e6b0b82269e1670280954d2e69bef2b86916eb64c31fc0be4d4833cdfc737b8280baa453584ec0631fa0ef924d4f'
